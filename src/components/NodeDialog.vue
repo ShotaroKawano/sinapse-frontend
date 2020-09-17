e9b3e2385b6ec8dbc12acdefde75484800b9a8d3
ShotaroKawano <shotaro.kawano009@gmail.com>
2020-09-17 14:34:41 +0900

    Add: スクレイピング機能を追加

--- src/components/NodeDialog.vue
+++ src/components/NodeDialog.vue
@@ -12,8 +12,9 @@
                 </div>
                 <input class="form-control" id="thumbnail" v-model="nodeForm.thumbnail"/>
                 <label for="approver">URL</label>
-                <input class="form-control" id="name" v-model="nodeForm.url"/>
-                <label for="name">Title(Name)</label>
+                <input class="form-control" id="url" v-model="nodeForm.url"/>
+                <label for="name">Title</label>
+                <!-- <label for="name">Title(Name)</label> -->
                 <input class="form-control" id="title" v-model="nodeForm.name"/>
                 <!-- <select class="form-control" id="thumbnail" v-model="nodeForm.thumbnail">
                     <option :key="'node-thumbnail-' + item.id" :value="item.id"
@@ -22,7 +23,8 @@
                         {{item.name}}
                     </option>
                 </select> -->
-                <label for="approver">Summary(Approver)</label>
+                <label for="approver">Summary</label>
+                <!-- <label for="approver">Summary(Approver)</label> -->
                 <input class="form-control" id="approver" v-model="nodeForm.summary"/>
                 <!-- <textarea class="form-control" id="approver" v-model="nodeForm.summary"></textarea> -->
                 <!-- <select class="form-control" id="approver" :value="nodeForm.approver.id"
@@ -34,13 +36,16 @@
             </div>
             <div class="footer">
                 <button @click="handleClickCancelSaveNode">Cancel</button>
-                <button @click="handleClickSaveNode">Ok</button>
+                <button @click="handleClickSaveNode">Save</button>
+                <button @click="handleClickGetInfo">Get</button>
             </div>
         </div>
     </div>
 </template>
 <script>
   import '../assets/modal.css';
+  // Ajax通信ライブラリ
+  import axios from 'axios';
 
   export default {
     props: {
@@ -61,13 +66,51 @@
       };
     },
     methods: {
+      handleClickGetInfo() {
+        // Json取得のベースURL
+        const URL_BASE = 'http://127.0.0.1:8000/newsapp/get';
+        // axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
+        // return axios.get(this.nodeForm.url)
+        // console.log(document.getElementsByName('csrf-token')[0].content)
+        console.log(this.nodeForm.url);
+        return axios({
+          method: 'POST',
+          url: URL_BASE,
+          data: {
+            url: this.nodeForm.url,
+          },
+          headers: {
+            'Content-Type': 'application/json;charset=UTF-8',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'POST PUT, DELETE, PATCH',
+            'xsrfHeaderName': 'X-CSRF-Token',
+            'withCredentials': true,
+            // 'X-CSRF-TOKEN': document.getElementsByName('csrf-token')[0].content
+            // 'X-CSRF-TOKEN': getCookieValue('XSRF-TOKEN')
+            // 'X-CSRF-TOKEN': csrf_token()
+          }
+        // }).post(URL_BASE, {
+          // withCredentials: true,
+        }).then((res) => {
+          console.log(res.data[0]);
+          this.nodeForm.thumbnail = res.data[0][2];
+          this.nodeForm.name = res.data[0][0];
+          this.nodeForm.summary = res.data[0][1];
+          // Vue.set(this, name, res.data);
+          // this.$emit('GET_AJAX_COMPLETE');
+        });
+      },
+      // save押下時に実行される
       handleClickSaveNode() {
+        console.log('koko');
         this.$emit('update:node', Object.assign(this.node, {
           name: this.nodeForm.name,
           thumbnail: this.nodeForm.thumbnail,
           summary: this.nodeForm.summary,
+          url: this.nodeForm.url,
           // approvers: [Object.assign({}, this.nodeForm.approver)],
         }));
+        // console.log(this.nodeForm.url);
         this.$emit('update:visible', false);
       },
       handleClickCancelSaveNode() {
--- vue.config.js
+++ vue.config.js
@@ -12,4 +12,11 @@ module.exports = {
     extract: false,
   },
   // publicPath: process.env.NODE_ENV === "production" ? "/flowchart-vue" : "/"
+  devServer: {
+    proxy: {
+      "http://127.0.0.1:8000/newsapp/": {
+        target: "http://localhost:3000",
+      }
+    }
+  }
 };
